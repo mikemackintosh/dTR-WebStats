57cdf0172bef4f8f59c5317b3601e3c3e46c93c7
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2013-01-06 19:33:02 -0800

    Create dtrws.js

---
+++ dtrws.js
@@ -0,0 +1,26 @@
+;(function( $ ) {
+
+  $.fn.dtrws = function( params ) {
+    
+    // params
+	  var profile = params;
+	  
+    // Create image
+	  var f=new Image(1,1);
+	  
+    // Web script
+	  var url = "/dtrws.php?";
+	  
+	  // Profile ID
+	  url += "profile="+profile
+	  
+	  // Screen Resolution
+	  url += "&resolutionx="+screen.width
+	  url += "&resolutiony="+screen.height
+
+    // Make request
+	  f.src=url;
+				
+  };
+
+})( jQuery );
