2996515fa4e8e413dfcdf7fffef82c6658d76021
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2013-01-24 00:45:05 -0500

    Update dtrws.js

--- dtrws.js
+++ dtrws.js
@@ -3,6 +3,9 @@
  *
  * author Mike Mackintosh <mike@bakeryframework.com>
  */
+
+var starttime = new Date();
+
 ;(function( $ ) {
 
   $.fn.dtrws = function( params ) {
@@ -23,6 +26,41 @@
 	  url += "&resolutionx="+screen.width
 	  url += "&resolutiony="+screen.height
 
+	  //
+	  url += "&loadtime="+(new Date().getTime() - starttime.getTime())
+
+	  // Flash Version
+	  //url += "&flashVer="+ (new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version').replace(/\D+/g, ',').match(/^,?(.+),?$/)[1]);
+
+	  // Domain
+	  url += "&domain="+ window.location.host
+	  
+	  // Protocol
+	  url += "&protocol="+ window.location.protocol
+
+	  url += "&pathname="+ window.location.pathname
+
+	  url += "&search="+ window.location.search
+
+	  // Location
+	  url += "&location="+ window.location.protocol +
+	    '//' + window.location.hostname +
+	    window.location.pathname +
+	    window.location.search
+
+	  // Page title
+	  url += "&title="+ $("title").text()
+	  
+	  // Referer
+	  url += "&refurl="+ document.referrer.split('/')[2]
+	  
+	  // 
+	  url += "&osVer="+navigator.appVersion
+	  
+	  url += "&osAgent="+navigator.userAgent
+
+	  
+	  
     // Make request
 	  f.src=url;
 				
